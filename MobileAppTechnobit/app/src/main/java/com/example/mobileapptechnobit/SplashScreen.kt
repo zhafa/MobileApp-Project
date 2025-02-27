aa1b87fc1b8ab28d99a35807dd3b4b0a9bf04ec5
SandroChrist <sibueasandro@gmail.com>
2025-02-27 12:46:11 +0700

    Update SplashScreen.kt

--- MobileAppTechnobit/app/src/main/java/com/example/mobileapptechnobit/SplashScreen.kt
+++ MobileAppTechnobit/app/src/main/java/com/example/mobileapptechnobit/SplashScreen.kt
@@ -1,29 +1,40 @@
 package com.example.mobileapptechnobit
 
-import android.os.Handler
-import android.os.Looper
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    var isVisible by remember { mutableStateOf(false) }
+    var startExitAnimation by remember { mutableStateOf(false) }
 
-    // Animasi Fade-in
+
+    val alpha by animateFloatAsState(
+        targetValue = if (startExitAnimation) 0f else 1f,
+        animationSpec = tween(200)
+    )
+    val scale by animateFloatAsState(
+        targetValue = if (startExitAnimation) 0.8f else 1f,
+        animationSpec = tween(200)
+    )
+
     LaunchedEffect(Unit) {
-        isVisible = true
-        delay(2000) // Splash screen selama 2 detik
+        delay(1200)
+        startExitAnimation = true
+        delay(10)
+
         navController.navigate(Screen.Walkthrough.route) {
             popUpTo(Screen.Splash.route) { inclusive = true }
         }
@@ -35,13 +46,14 @@ fun SplashScreen(navController: NavController) {
             .background(Color(0xFF2752E7)), // Warna biru sesuai gambar
         contentAlignment = Alignment.Center
     ) {
-        AnimatedVisibility(visible = isVisible) {
-            Text(
-                text = "Arunika",
-                color = Color.White,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
+        Text(
+            text = "Arunika",
+            color = Color.White,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .scale(scale)
+                .alpha(alpha)
+        )
     }
 }
